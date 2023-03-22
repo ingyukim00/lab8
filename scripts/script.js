1724dc686b6395861b05f5f77928356b31bbc5f1
ingyukim <ikim59@bcit.ca>
2023-03-21 18:34:07 -0700

    Create function findCourse(courseList)

--- scripts/script.js
+++ scripts/script.js
@@ -66,3 +66,44 @@ function createCourseArray() {
 console.log(createCourseArray());
 
 let CourseList2 = createCourseArray();
+
+function findCourse(CourseList2) {
+    let result3 = 0;
+    let user_input;
+    let new_item;
+    let result4 = 0;
+    let i = 0;
+    const CourseListUlList = document.querySelectorAll(".courses_list ul li");
+
+    do {
+        user_input = prompt("Type fore digit number: ");
+        if (isNaN(user_input) === false && user_input.length === 4) {
+            result3 = 1;
+        }
+    } while (result3 != 1);
+
+    for (let course of CourseList2) {
+        if (course["code"].slice(5, 9) === user_input) {
+            result4 = 1;
+            document.querySelectorAll(".courses_list ul li")[i].style.backgroundColor = "green";
+        };
+        i += 1;
+    };
+
+    if (result4 === 0) {
+        const new_item = {code: `${user_input} N/A`, date: "Fall 2020"};
+        const new_list = document.createElement("li");
+        const new_a = document.createElement("a");
+        new_a.setAttribute("href", "N/A");
+        new_a.textContent = new_item.code;
+        const new_span = document.createElement("span");
+        new_span.textContent = new_item.date;
+        const sampleCourse = new_item;
+        console.log(sampleCourse);
+        new_list.appendChild(new_a);
+        new_list.appendChild(new_span);
+        document.querySelector(".courses_list ul").appendChild(new_list);
+    };
+};
+
+findCourse(CourseList2);
